37039b08441cf97b87a690792ce7017d331ae01f
Mike Cann <mike.cann@gmail.com>
2025-11-07 14:33:50 +0800

    wip

--- example/convex/_generated/api.d.ts
+++ example/convex/_generated/api.d.ts
@@ -8,10 +8,6 @@
  * @module
  */
 
-import type * as lib3_builder from "../lib3/builder.js";
-import type * as lib3_middleware from "../lib3/middleware.js";
-import type * as lib3_types from "../lib3/types.js";
-import type * as lib3_zod_support from "../lib3/zod_support.js";
 import type * as myFunctions from "../myFunctions.js";
 import type * as zodExample from "../zodExample.js";
 
@@ -30,10 +26,6 @@ import type {
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  "lib3/builder": typeof lib3_builder;
-  "lib3/middleware": typeof lib3_middleware;
-  "lib3/types": typeof lib3_types;
-  "lib3/zod_support": typeof lib3_zod_support;
   myFunctions: typeof myFunctions;
   zodExample: typeof zodExample;
 }>;
