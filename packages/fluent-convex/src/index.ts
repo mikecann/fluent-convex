03ed2f3aa302a97d201d4d180ca42063f99baec6
Mike Cann <mike.cann@gmail.com>
2026-02-10 14:51:36 +0800

    remove ConvexMiddlewareOptions type export from index.ts and clean up middleware.ts interface

--- packages/fluent-convex/src/index.ts
+++ packages/fluent-convex/src/index.ts
@@ -5,7 +5,6 @@ export { ConvexBuilderWithHandler } from "./ConvexBuilderWithHandler";
 
 export type {
   ConvexMiddleware,
-  ConvexMiddlewareOptions,
   AnyConvexMiddleware,
 } from "./middleware";
 
--- packages/fluent-convex/src/middleware.ts
+++ packages/fluent-convex/src/middleware.ts
@@ -1,10 +1,5 @@
 import { Context, Promisable } from "./types";
 
-export interface ConvexMiddlewareOptions<TInContext extends Context> {
-  context: TInContext;
-  next: <U extends Context>(context: U) => Promisable<{ context: U }>;
-}
-
 export interface ConvexMiddleware<
   TInContext extends Context,
   TOutContext extends Context,
